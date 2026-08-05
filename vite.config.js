6429a769c65e860c2a86d74bc0f8bb972d5f3942
Labade Raphael <raphael9157@gmail.com>
2026-08-05 11:22:51 +0100

    Fix the env issue

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: { '/api': { target: process.env.VITE_API_URL || 'http://localhost:5000', changeOrigin: true } },
+    proxy: { '/api': { target: import.meta.env.VITE_API_URL, changeOrigin: true } },
   },
 })
